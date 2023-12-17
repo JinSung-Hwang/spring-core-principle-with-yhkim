f8df65a364510b3a59b9a367f83eb42b019965f7
JinSung-Hwang <wlstjd7782@gmail.com>
2023-12-17 22:57:37 +0900

    feature: 섹션3 - 스프링 핵심 원리 이해2 - 관심사의 분리

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -3,6 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,11 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
-  RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+  RateDiscountPolicy discountPolicy;
+  @BeforeEach
+  public void beforeEach() {
+    discountPolicy = new RateDiscountPolicy();
+  }
 
   @Test
   @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
