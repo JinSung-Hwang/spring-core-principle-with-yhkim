c01d4e76a8433293d6abd8eff8ea74ebba8e7de6
JinSung-Hwang <wlstjd7782@gmail.com>
2023-12-21 23:11:41 +0900

    feature: 섹션3 - 스프링 핵심 원리 이해2 - 스프링으로 전환하기

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,20 +9,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+  @Bean
   public MemberService memberService() { // note: 메서드명과 리턴타입이 역할(배역)을 담당한다.
     return new MemberServiceImpl(memberRepository()); // note: 내부 로직이 구현을 담당한다.
   }
 
+  @Bean
   private static MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public OrderService orderService() {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
+  @Bean
   public DiscountPolicy discountPolicy() {
 //    return new FixDiscountPolicy();
     return new RateDiscountPolicy(); // note: 할인 정책을 바꾸려면 간단히 구현체만 바꿔주면 된다.
