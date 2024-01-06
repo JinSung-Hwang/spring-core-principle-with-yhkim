dd2d11bfbd7360985115eb83d78828d75177d522
JinSung-Hwang <wlstjd7782@gmail.com>
2024-01-06 23:57:46 +0900

    feature: 섹션5 - 싱글톤 컨테이너 - @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,16 +16,19 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
   @Bean
   public MemberService memberService() { // note: 메서드명과 리턴타입이 역할(배역)을 담당한다.
+    System.out.println("call AppConfig.memberService");
     return new MemberServiceImpl(memberRepository()); // note: 내부 로직이 구현을 담당한다. // note: 메서드의 리턴타임이 MemberService인데 실제 리턴타입은 MemberServiceImpl이다. 이렇게 작성해도 상관없으며 실제로 빈등록될떄는 MemberServiceImpl로 치환되어서 등록된다.
   }
 
   @Bean
-  private static MemberRepository memberRepository() {
+  public MemberRepository memberRepository() {
+    System.out.println("call AppConfig.memberRepository");
     return new MemoryMemberRepository();
   }
 
   @Bean
   public OrderService orderService() {
+    System.out.println("call AppConfig.orderService");
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
