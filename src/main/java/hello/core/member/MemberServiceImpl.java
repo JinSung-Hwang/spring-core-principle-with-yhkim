908ec631a24c800b4779d8a017273052764b1330
JinSung-Hwang <wlstjd7782@gmail.com>
2024-01-09 23:12:35 +0900

    feature: 섹션6 - 컴포넌트 스캔 - 컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
   private final MemberRepository memberRepository;
 
+  @Autowired // note: 컴포넌트 스캔으로 빈을 등록할때 의존성을 주입하려면 @Autowired를 같이써야한다. @Autowired 는 ac.getBean(MemberRepository.class) 와 같은 코드이다.
   public MemberServiceImpl(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
   }
