a8bf296435ed66d93bbf4753c24e93d9ef3d1230
JinSung-Hwang <wlstjd7782@gmail.com>
2024-01-21 23:26:23 +0900

    feature: 섹션7 - 의존관계 자동 주입 - 자동, 수동의 올바른 실무 운영 기준

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,6 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// note: 자동 빈 등록은 언제 사용하는것이 좋을까?
+// note: 자동빈등록은 비지니스로직(Controller, Service, DAO)을 담당하는 bean들에 사용하는것이 좋다.
+// note: 이런빈들 비지니스로직이 증가함에 따라서 빈객체와 의존성이 다양해지기 때문에 직접 설정하는것보다 자동빈 설정하는것이 편하다.
+
+// note: 수동 빈 등록은 언제 사용하는것이 좋을까?
+// note: 수동빈 등록은 기술 지원을 담당하는 빈객체의 경우 수동빈으로 등록하는 것이 좋다.
+// note: 수동빈 등록하면 빈이 어떤 의존성을 갖고 있는지 한번에 파악할 수 있는 장점이 있다.
+// note: 이런 수동빈 등록 장점을 활용하여 기술지원 빈들이 어떤 의존성을 갖고 있고 어떤 설정이 되어있는지 한눈에 파악할 수 있다.
+
+// note: 번외로 전략패턴을 사용하는 빈의 경우 수동빈으로 사용하여 의존성을 한눈에 파악할 수 있도록 만들기도하고 아니면
+// note: 전략패턴으로 사용되는 '빈을 자동 빈 등'록하고 하나의 디렉토리에 모아서 관리하여 한눈에 할 수 있도록 하기도 한다.
+
 @Configuration // note: @Configuration 애노테이션을 안붙이면 ConfigurationSingletonTest.java에 설명되어있는 것처럼 CGLIB 기술로 자식 객체가 생성되지 않는다.
 // note: 하여 아래 사용된 @Bean으로 생성되는 객체들이 빈으로 등록이 되고 생성이 되지만 싱글톤을 보장하지는 않는다.
 public class AppConfig {
