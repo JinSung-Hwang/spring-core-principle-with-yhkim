e0ec1df620719355b8ca331f71c7b5314ccfb76a
JinSung-Hwang <wlstjd7782@gmail.com>
2024-01-24 18:34:25 +0900

    feature: 섹션8 - 빈 생명주기 콜백 - 빈 등록 초기화, 소멸 메서드

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,13 @@ public class BeanLifeCycleTest {
 
   @Configuration
   static class LifeCycleConfig {
-    @Bean
+    // note: version2 빈 생성과 소멸 관리
+    // note: 빈에 initMethod, destoryMethod 지정 방식의 장점
+    // note: 1. 메서드 이름을 자유롭게 지정할 수 있다.
+    // note: 2. 빈 객체가 스프링 코드에 의존하지 않는다. (인터페이스를 구현하지 않아도 된다.)
+    // note: 3. 코드가 아니라 설정 정보를 등록하는 방식이기때문에 빈 생명주기 시점에 외부라이브러리 메서드를 호출 할 수 있다.
+    @Bean(initMethod = "init", destroyMethod = "close") // note: version2 빈 생성과 소멸 관리
+    // note: destroyMethod는 기본값이 (inferred) 추론이라는 기능이있는데 등록된 빈의 close() 또는 shutdown()이라는 이름의 메서드를 빈 소멸시 자동으로 호출해준다. 추론을 사용하지 않을땐 "" 빈문자열을 넣어준다.
     public NetworkClient networkClient() {
       NetworkClient networkClient = new NetworkClient();
       networkClient.setUrl("http://hello-spring.dev");
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,8 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+//public class NetworkClient implements InitializingBean, DisposableBean { // note: version1 빈 생성과 소멸 관리
+public class NetworkClient {
   // note: InitializingBean, DisposableBean를 활용하여 빈 콜백 생명주기 다루는 방법
   // note:   1. 인터페이스는 스프링 전용 인터페이스라서 스프링에 의존적이라는 단점이 있다.
   // note:   2. 내가 코드를 고칠 수 없는 외부 라이브러리에는 적용할 수 없다.
@@ -31,16 +32,32 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     System.out.println("close: " + url);
   }
 
-  @Override
-  public void afterPropertiesSet() throws Exception { // note: 의존 관계 주입이 끝나면 호출된다.
+
+  // note: version1 생성과 소멸 관리
+//  @Override
+//  public void afterPropertiesSet() throws Exception { // note: 의존 관계 주입이 끝나면 호출된다.
+//    System.out.println("NetworkClient.afterPropertiesSet");
+//    connect();
+//    call("초기화 연결 메시지");
+//  }
+//
+//  @Override
+//  public void destroy() throws Exception { // note: 빈이 종료될 때 호출된다.
+//    System.out.println("NetworkClient.destroy");
+//    disconnect();
+//  }
+
+
+  // note: version2 빈 생성과 소멸 관리
+  public void init() throws Exception { // note: 의존 관계 주입이 끝나면 호출된다.
     System.out.println("NetworkClient.afterPropertiesSet");
     connect();
     call("초기화 연결 메시지");
   }
 
-  @Override
-  public void destroy() throws Exception { // note: 빈이 종료될 때 호출된다.
+  public void close() throws Exception { // note: 빈이 종료될 때 호출된다.
     System.out.println("NetworkClient.destroy");
     disconnect();
   }
+
 }
