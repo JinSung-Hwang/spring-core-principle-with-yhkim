c3178d6dd563bc0aec51b49deeca1cce1f410209
JinSung-Hwang <wlstjd7782@gmail.com>
2024-02-01 23:51:39 +0900

    feature: 섹션9 - 빈 스코프 - 스코프와 Provider

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,16 +11,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-  private final  LogDemoService logDemoService;
-  private final MyLogger myLogger; // note: MyLogger는 리퀘스트 스코프 빈으로 생성되었다.(@Scope(value = "request"))하여 Http요청되어야 빈이 생성되기때문에 이 LogDemoController에서는 주입할 수가 없어서 에러가 난다.
+  private final LogDemoService logDemoService;
+//  private final MyLogger myLogger; // note: MyLogger는 리퀘스트 스코프 빈으로 생성되었다.(@Scope(value = "request"))하여 Http요청되어야 빈이 생성되기때문에 이 LogDemoController에서는 주입할 수가 없어서 에러가 난다.
+  private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+  // note: MyLogger에 ObjectProvider로 감싸면 myLogger를 찾을 수 있는 디펜던시 MyLogger가 주입된다.
 
   @RequestMapping("log-demo")
   @ResponseBody
-  public String logDemo(HttpServletRequest request) {
+  public String logDemo(HttpServletRequest request) throws InterruptedException {
     String requestURL = request.getRequestURL().toString();
+    MyLogger myLogger = myLoggerObjectProvider.getObject();
     myLogger.setRequestURL(requestURL);
 
     myLogger.log("controller test");
+    Thread.sleep(1000);
     logDemoService.logic("testId");
     return "OK";
   }
